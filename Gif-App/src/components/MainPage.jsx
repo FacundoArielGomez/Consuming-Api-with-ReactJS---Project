d67f84e26f5b986a9b34149bb054871d5eecb928
FacundoArielGomez <facuarielgomezz@gmail.com>
2023-02-28 08:45:38 -0300

    add aesthetic loading

--- Gif-App/src/components/MainPage.jsx
+++ Gif-App/src/components/MainPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './mainPage.css'
+import {Spinner} from './Spinner'
 
 export const MainPage = ({gifs, loading}) => {
     return (
         <main className="gifs-wrapper">
             {
-            loading ? <h2>LOADING</h2> :
+            loading ? <Spinner /> :
                 gifs?.map(gif => {
                     return(
                     <div className="single-gif" key={gif.idGif}>
---
+++ Gif-App/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import './spinner.css'
+
+export const Spinner = ()=>{
+    return (
+        <section className='spinner-div'>
+        <span class="loader"></span>
+        </section>
+    )
+}
