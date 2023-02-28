c9a33799519cb4e3b0acf0719977178732b7cf55
FacundoArielGomez <facuarielgomezz@gmail.com>
2023-02-28 18:51:04 -0300

    fix a sintax error in spinner, className instead class

--- Gif-App/src/components/Spinner.jsx
+++ Gif-App/src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import './spinner.css'
 export const Spinner = ()=>{
     return (
         <section className='spinner-div'>
-        <span class="loader"></span>
+        <span className="loader"></span>
         </section>
     )
 }
