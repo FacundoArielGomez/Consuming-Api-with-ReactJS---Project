74c81f69d9685ecdb4ee3aa7728ae556c4057cdc
FacundoArielGomez <facuarielgomezz@gmail.com>
2023-03-02 15:35:11 -0300

    Changed class name "Empty search" yo "Message main div"

--- Gif-App/src/components/EmptySearch.jsx
+++ Gif-App/src/components/EmptySearch.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import './emptySearch.css'
+import './messageMainDiv.css'
 
 export function EmptySearch(){
     return(
-        <section className="empty-search">
+        <section className="message-main-div">
             <h2>Search a gif! All are amazing</h2>
             <img src="https://media.giphy.com/media/l3vRlT2k2L35Cnn5C/giphy-downsized.gif"></img>
         </section>
--- Gif-App/src/components/ErrorDiv.jsx
+++ Gif-App/src/components/ErrorDiv.jsx
@@ -1,8 +1,8 @@
-import './emptySearch.css'
+import './messageMainDiv.css'
 export function ErrorDiv ({error}){
 
     return(
-        <section className='empty-search'>
+        <section className='message-main-div'>
             <h2>There was an error!</h2>
             <p>{error}</p>
         </section>
