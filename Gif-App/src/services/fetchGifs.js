82378c8deff41d3881034ccf1e166907fe7fe489
FacundoArielGomez <facuarielgomezz@gmail.com>
2023-03-04 15:37:59 -0300

    fix sintax error, try/catch instead .catch

--- Gif-App/src/services/fetchGifs.js
+++ Gif-App/src/services/fetchGifs.js
@@ -3,7 +3,6 @@ const apiKeys = 'ur1RjBt1v1TO5PHKikS4b97ex97wS0PR'
 export async function fetchGifs(query){
     const UrlGif= `https://api.giphy.com/v1/gifs/search?api_key=${apiKeys}&q=${query}&limit=10&offset=0&rating=g&lang=en`
   
-    try{
     return fetch(UrlGif).
         then(response => response.json()).
         then(gifsResponse => {
@@ -12,14 +11,14 @@ export async function fetchGifs(query){
             const idGif = gif.id;
             const imageGif = gif.images.fixed_height.url;
             const titleGif = gif.title;
-            
+            console.log('3er cambio')
                 
             return {idGif, imageGif, titleGif}
     })
     return namedGifs  
 }
-)
-}catch(e){
+).catch(e => {
     throw new Error('Error searching gifs')
-}
+})
+
 }
