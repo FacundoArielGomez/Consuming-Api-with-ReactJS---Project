8da32e072985461a8376d2f8cc8d46daa4025252
FacundoArielGomez <facuarielgomezz@gmail.com>
2023-03-30 19:22:34 -0300

    fix error router

--- Gif-App/src/pages/MoreInfo/MoreInfo.jsx
+++ Gif-App/src/pages/MoreInfo/MoreInfo.jsx
@@ -7,30 +7,33 @@ import { GifsContext } from '../../context/gifsContext/GifsProvider'
 export function MoreInfo (){
 
     const {id} = useParams()
+    const idParam = id
 
-    const {gifs} = useContext(GifsContext)
+    const {gifsContext} = useContext(GifsContext)
 
     const gifsLocalStorage = JSON.parse(localStorage.getItem("gifs"))
 
-    const getSingleGif = (id)=>{
+    const getSingleGif =  async (id)=>{
         return fetch(`https://api.giphy.com/v1/gifs/${id}?api_key=ur1RjBt1v1TO5PHKikS4b97ex97wS0PR`)
         .then(res => res.json())
         .then(response => {
+           
+            console.log(response)
             const {data} = response
             const {title, id} = data
             const {url} = data.images.fixed_height
 
-            return ({'imageGif': url, 'titleGif': title, idGif: id})
+            return ({'imageGif': url, 'titleGif': title, 'idGif': id})
         })
     }
 
-    const renderGifs = gifs?.find(gif => gif.idGif === id) || gifsLocalStorage?.find(gif => gif.idGif === id)
+    const renderGifs = gifsContext?.find(gif => gif.idGif === idParam) || gifsLocalStorage?.find(gif => gif.idGif === idParam)
 
     const [singleGif, setSingleGif] = useState(renderGifs)
 
     useEffect(()=>{
-        if(!singleGif){
-            getSingleGif(id)
+        if(!renderGifs){
+            getSingleGif(idParam)
             .then(gif => {setSingleGif(gif)})
         }
     },[])
